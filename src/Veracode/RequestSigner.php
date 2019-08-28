121472b4cf65f8726dec38b7c16ad3d1a5d61580
Boy Baukema <boy@relaxnow.nl>
2019-08-28 16:37:18 +0200

    Cleaned up HmacRequestSigner
    Improved phpdoc and removed var_dumps

--- src/Veracode/RequestSigner.php
+++ src/Veracode/RequestSigner.php
@@ -2,31 +2,41 @@
 
 namespace Veracode;
 
+/**
+ * Direct port of Java HmacRequestSigner
+ */
 final class HmacRequestSigner
 {
-    // Included in the signature to inform Veracode of the signature version.
+    /**
+     * Included in the signature to inform Veracode of the signature version.
+     */
     private static $VERACODE_REQUEST_VERSION_STRING = "vcode_request_version_1";
 
-    // Expected format for the unencrypted data string.
+    /**
+     * Expected format for the unencrypted data string.
+     */
     private static $DATA_FORMAT = "id=%s&host=%s&url=%s&method=%s";
 
-    // Expected format for the Authorization header.
+    /**
+     * Expected format for the Authorization header.
+     */
     private static $HEADER_FORMAT = "%s id=%s,ts=%s,nonce=%s,sig=%s";
 
-    // Expect prefix to the Authorization header.
+    /**
+     * Expect prefix to the Authorization header.
+     */
     private static $VERACODE_HMAC_SHA_256 = "VERACODE-HMAC-SHA-256";
 
-    // Charset to use when encrypting a string.
+    /**
+     * Charset to use when encrypting a string.
+     */
     private static $UTF_8 = "UTF-8";
 
-    // Private constructor.
-    private function HmacRequestSigner()
-    {
-        /*
-         * This is a utility class that should only be accessed through its
-         * static methods.
-         */
-    }
+    /**
+     * This is a utility class that should only be accessed through its
+     * static methods.
+     */
+    private function __constructor(){}
 
     /**
      * Entry point for HmacRequestSigner. Returns the value for the
@@ -59,7 +69,6 @@ final class HmacRequestSigner
         $timestamp = round(microtime(true) * 1000);
         $nonce = bin2hex(random_bytes(16));
         $signature = self::getSignature($key, $data, $timestamp, $nonce);
-        var_dump($signature);
 
         return sprintf(self::$HEADER_FORMAT, self::$VERACODE_HMAC_SHA_256, $id, $timestamp, $nonce, $signature);
     }
@@ -70,22 +79,18 @@ final class HmacRequestSigner
      */
     private static function getSignature($key, $data, $timestamp, $nonce)
     {
-        var_dump("Key: $key");
         $keyBytes = hex2bin($key);
-        var_dump("nonce: $nonce");
         $nonceBytes = hex2bin($nonce);
         $encryptedNonce = self::hmacSha256($nonceBytes, $keyBytes);
-        var_dump("encryptedNonce: " . bin2hex($encryptedNonce));
         $encryptedTimestamp = self::hmacSha256($timestamp, $encryptedNonce);
-        var_dump("encryptedTimestamp: " . bin2hex($encryptedTimestamp));
         $signingKey = self::hmacSha256(self::$VERACODE_REQUEST_VERSION_STRING, $encryptedTimestamp);
-        var_dump("signingKey: " . bin2hex($signingKey));
         $signature = self::hmacSha256($data, $signingKey);
-        var_dump("signature: " . bin2hex($signature));
         return bin2hex($signature);
     }
 
-    // Encrypt a string using the provided key.
+    /**
+     * Encrypt a string using the provided key.
+     */
     private static function hmacSha256($data, $key)
     {
         return hash_hmac("sha256", $data, $key, true);
